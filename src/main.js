11e10eff7d3da1bcbd20a88418b9fb5419c0cd44
“isaMoratogit config --global user.email “morato.delacalle@bootcamp-upgrade.com <morato.delacalle@bootcamp-upgrade.com>
2021-11-12 19:47:02 +0100

    feat main

--- src/main.js
+++ src/main.js
@@ -1,4 +1,22 @@
+const express = require('express')
+const { connectWithDb } = require('./utils/db/db')
 
+
+const PORT = 3000;
+const app = express();
+
+connectWithDb();
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+
+app.use('*', (req, res) => {
+    return res.status(404).send('Route not found')
+})
+
+app.listen(PORT, () => {
+    console.log('Server is running in http://localhost:'+PORT)
+})
 
 
 

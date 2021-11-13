0cadab6ad26d3877dd6826cd42c6d1f60c20ccd8
Daniel López <daniel.lp1094@gmail.com>
2021-11-13 12:52:11 +0100

    FEAT: defined method get by id and route

--- src/routes/candidate.routes.js
+++ src/routes/candidate.routes.js
@@ -1,8 +1,9 @@
 const CandidateRoutes = require('express').Router();
-const { getAllCandidates } = require('../controllers/candidate.controller');
+const { getAllCandidates, getCandidateById } = require('../controllers/candidate.controller');
 
 
 CandidateRoutes.get('/', getAllCandidates);
+CandidateRoutes.get('/:id', getCandidateById);
 
 
 module.exports = CandidateRoutes;

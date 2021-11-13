5419b6e2ab9cf17d61307c5e8061b2a66bb1359e
Daniel López <daniel.lp1094@gmail.com>
2021-11-13 13:44:30 +0100

    FEAT: defined method delete and route

--- src/routes/candidate.routes.js
+++ src/routes/candidate.routes.js
@@ -1,12 +1,13 @@
 const CandidateRoutes = require('express').Router();
 
-const { getAllCandidates, getCandidateById, postNewCandidate, putCandidateUpdate } = require('../controllers/candidate.controller');
+const { getAllCandidates, getCandidateById, postNewCandidate, putCandidateUpdate, deleteCandidate } = require('../controllers/candidate.controller');
 
 
 CandidateRoutes.get('/', getAllCandidates);
 CandidateRoutes.get('/:id', getCandidateById);
 CandidateRoutes.post('/', postNewCandidate);
 CandidateRoutes.put('/:id', putCandidateUpdate);
+CandidateRoutes.delete('/:id', deleteCandidate);
 
 
 module.exports = CandidateRoutes;

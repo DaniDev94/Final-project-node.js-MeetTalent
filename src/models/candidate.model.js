185a8fd1ad7a7432eedccdf14548b3d223d32850
Daniel López <daniel.lp1094@gmail.com>
2021-11-13 02:02:56 +0100

    FEAT: candidate schema created

--- src/models/candidate.model.js
+++ src/models/candidate.model.js
@@ -1,2 +1,91 @@
+const mongoose = require('mongoose');
+
+const cadidateSchema = new mongoose.Schema({
+    image: {
+        name: {
+            type: String,
+            trim: true,
+        },
+        url: {
+            type: String,
+            unique: true,
+            trim: true
+        }
+    },
+    name: {
+        first: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        last: {
+            type: String,
+            required: true,
+            trim: true
+        },
+    },
+    profession: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    networks: {
+        type: [String],
+        unique: true,
+        trim: true
+    },
+    information: {
+        age: {
+            type: Number,
+            required: true,
+            trim: true,
+            min: 18
+        },
+        address: {
+            city: {
+                type: String,
+                required: true,
+                trim: true
+            },
+            locality: {
+                type: String,
+                required: true,
+                trim: true
+            },
+            pc: {
+                type: String,
+                required: true,
+                trim: true
+            }
+        },
+        phone: {
+            type: Number,
+            required: true,
+            unique: true,
+        },
+        keywords: {
+            type: [String],
+            trim: true,
+        },
+        training: {
+            type: {
+                type: String,
+                required:true,
+                trim: true
+            },
+            name: {
+                type: String,
+                required: true,
+                trim: true
+            },
+            center: {
+                type: String,
+                trim: true
+            }
+        }
+    }
+},{timestamps:true});
 
 
+const Candidate = mongoose.model('candidates', cadidateSchema);
+module.exports = Candidate;

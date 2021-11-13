bf8af202f9ca0fe12f69baff41510af77585e3ca
Aitor Olivar <aitor.olivar@bootcamp-upgrade.com>
2021-11-13 17:02:18 +0100

    FEAT: changes routes

--- src/routes/joboffer.routes.js
+++ src/routes/joboffer.routes.js
@@ -1,11 +1,16 @@
 const JobOfferRoutes = require("express").Router();
 
-const {getJobOffer} = require('../controllers/joboffer.controller');
+const {getJobOffer, postNewJobOffer, putJobOffer, pachJobOffer, deleteJobOffer, getJobOfferById} = require('../controllers/joboffer.controller');
 
-JobOfferRoutes.get('/', getJobOffer);
+JobOfferRoutes.get('/',getJobOffer)
+JobOfferRoutes.post('/',postNewJobOffer)
+JobOfferRoutes.put('/:id',putJobOffer)
+JobOfferRoutes.patch('/:id',pachJobOffer)
+JobOfferRoutes.delete('/:id',deleteJobOffer)
+JobOfferRoutes.get('/:id',getJobOfferById)
+
 
 module.exports = JobOfferRoutes;
-
 
 
 

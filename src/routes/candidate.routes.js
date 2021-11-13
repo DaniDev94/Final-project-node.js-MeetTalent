80dc70c8557923e769aa08d2c054f522ff0ba397
Daniel López <daniel.lp1094@gmail.com>
2021-11-13 13:33:51 +0100

    FEAT: defined method put and route

--- src/routes/candidate.routes.js
+++ src/routes/candidate.routes.js
@@ -1,11 +1,12 @@
 const CandidateRoutes = require('express').Router();
 
-const { getAllCandidates, getCandidateById, postNewCandidate } = require('../controllers/candidate.controller');
+const { getAllCandidates, getCandidateById, postNewCandidate, putCandidateUpdate } = require('../controllers/candidate.controller');
 
 
 CandidateRoutes.get('/', getAllCandidates);
 CandidateRoutes.get('/:id', getCandidateById);
 CandidateRoutes.post('/', postNewCandidate);
+CandidateRoutes.put('/:id', putCandidateUpdate);
 
 
 module.exports = CandidateRoutes;

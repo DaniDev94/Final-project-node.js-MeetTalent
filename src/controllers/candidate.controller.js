6cb5f93bbd2b8f734caa21ea6b6fb17ecd6c8e22
Daniel López <daniel.lp1094@gmail.com>
2021-11-13 12:47:58 +0100

    FEAT: sarch candidate by id

--- src/controllers/candidate.controller.js
+++ src/controllers/candidate.controller.js
@@ -11,7 +11,19 @@ const getAllCandidates = async (req, res, next) => {
     }
 }
 
+const getCandidateById = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const candidateById = await Candidate.findById(id);
+        return res.status(200).json(candidateById)
+    } catch (err) {
+        err.message = 'No candidate found with this id';
+        return next(`Error: ${err}.`);
+    }
+}
+
 
 module.exports = {
-    getAllCandidates
+    getAllCandidates,
+    getCandidateById
 }

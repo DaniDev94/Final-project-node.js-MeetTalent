541cdac8019e6fec341a243ffa2856e3d9cf60a8
Daniel López <daniel.lp1094@gmail.com>
2021-11-13 03:46:08 +0100

    FEAT: add path of candidate

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const EnterpriseRoutes = require('./routes/enterprise.routes');
+const CandidateRoutes = require('./routes/candidate.routes');
+
 const { connectWithDb } = require('./utils/db/db');
 
 
@@ -11,6 +13,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use('/enterprise', EnterpriseRoutes);
+app.use('/candidates', CandidateRoutes);
 
 
 app.use('*', (req, res) => {

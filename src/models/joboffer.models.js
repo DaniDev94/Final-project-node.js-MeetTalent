8126e4696a4f27f0a35f5f3151f6a1ed5741bc3e
Aitor Olivar <aitor.olivar@bootcamp-upgrade.com>
2021-11-13 18:37:45 +0100

    FEAT: changes joboffer

--- src/models/joboffer.models.js
+++ src/models/joboffer.models.js
@@ -2,89 +2,93 @@ const mongoose = require('mongoose');
 
 const jobofferSchema = new mongoose.Schema(
     {
-        city: {type:String, 
-               required: true, 
-               unique:true, 
-               trim:true},
-        country: {type:String, 
-                  required: true, 
-                  unique:true, 
-                  trim:true},
-        availability: {type:String, 
-                       required:true, 
-                       unique:true,  
-                       trim:true},
-        
+
+        city: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        country: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        availability: {
+            type: String,
+            trim: true
+        },
+
         //condiciones
-    
-    terms: {
-        salary: {
-            type: Number,
-            required:true,
-            trim:true
+
+        terms: {
+            salary: {
+                type: Number,
+                required: true,
+                trim: true
+            },
+            workingDay: {
+                type: String,
+                required: true,
+                trim: true
+            },
+            contract: {
+                type: String,
+                required: true,
+                trim: true
+            },
+            
+            //descripción de la oferta
+
+            offerDescription: {
+                descriptionOfRequirements: {
+                    type: String,
+                    required: true,
+                    trim: true
+                },
+                internalEncodings: {
+                    type: String,
+                    required: true,
+                    trim: true
+                }
+            },
+
+            //confirmación
+            confirmation: {
+                offerTitle: {
+                    type: String,
+                    required: true,
+                    trim: true
+                },
+                vacancies: {
+                    type: Number,
+                    required: true,
+                    trim: true
+                },
+                language: {
+                    type: String,
+                    required: true,
+                    trim: true
+                },
+                sector: {
+                    type: String,
+                    required: true,
+                    trim: true
+                },
+                training: {
+                    type: String,
+                    required: true,
+                    trim: true
+                },
+                offerDescriptionEnterprise: {
+                    type: String,
+                    required: true,
+                    trim: true
+                }
+            }
         },
-        workingDay: {
-            type: String,
-            required:true,
-            trim:true
-        },
-        contract: {
-            type: String,
-            required: true,
-            trim: true
-        }
+
     },
 
-    //descripción de la oferta
-
-    offerDescription: {
-        descriptionOfRequirements: {
-            type: String,
-            required: true,
-            trim: true
-        },
-        internalEncodings: {
-            type: String,
-            required: true,
-            trim: true
-        }
-    },
-
-    //confirmación
-    confirmation: {
-        offerTitle: {
-            type: String,
-            required:true,
-            unique:true,
-            trim: true
-        },
-        vacancies:{
-            type: Number,
-            required:true,
-            trim:true
-        },
-        language: {
-            type: String,
-            required: true,
-            trim: true
-        },
-        sector: {
-            type: String,
-            required: true,
-            trim: true
-        },
-        training: {
-            type: String,
-            required: true,
-            trim: true
-        },
-        offerDescriptionEnterprise: {
-            type: String,
-            required: true,
-            trim:  true
-        }
-    }
-},
 )
 
 

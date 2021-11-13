14810d358838ca5e06c754687900e0a85497b1c4
Aitor Olivar <aitor.olivar@bootcamp-upgrade.com>
2021-11-13 16:39:20 +0100

    FEAT: changes joboffer controller.js

--- src/controllers/joboffer.controller.js
+++ src/controllers/joboffer.controller.js
@@ -1,4 +1,4 @@
-const JobOffer = require('../models/joboffer.model');
+const JobOffer = require('../models/joboffer.models');
 
 
 
@@ -11,4 +11,70 @@ const getJobOffer = async(req, res, next) => {
         return next(`Error:${err}`);
     }
 };
+const postNewJobOffer = async(req, res, next) => {
+    try{
+        const newEnterprise = new JobOffer(req.body);
+        const JobOffersave = await newJoboffer.save()
+        return res.status(200).json(JobOffersave)
+    }catch(err){
+        err.message = 'The new hob offer cannot created';
+        return next (`Error:${err}`)
+    }
+}
 
+const putJobOffer = async (req, res, next) => {
+    try{
+        const {id} = req.params;
+
+        const otherJobOffer=new JobOffer(req.body);
+        otherJobOffer._id = id;
+        const updateJobOffer= await JobOffer.findByIdAndUpdate(id, otherJobOffer);
+        return res.status(200).json(updateJobOffer);
+    }catch(err){
+        err.message = 'Job Offer cannot be update';
+        return next (`Error:${err}`)
+    }
+}  
+
+const pachJobOffer =  async (req, res, next) => {
+    try{
+        const {id} = req.params;
+
+        const otherJobOffer=new JobOffer(req.body);
+        otherJobOffer._id = id;
+        const updateJobOffer= await JobOffer.findByIdAndUpdate(id, otherJobOffer)
+        return res.status(200).json(updateJobOffer);
+    }catch(err){
+        err.message = 'JobOffer cannot be update';
+        return next (`Error:${err}`)
+    }
+}   
+const deleteJobOffer =  async (req, res, next) => {
+    try{
+        const {id} = req.params;
+        const deleteJobOffer= await JobOffer.findByIdAndDelete(id)
+        return res.status(200).json(deleteJobOffer);
+    }catch(err){
+        err.message = 'JobOffer cannot be update';
+        return next (`Error:${err}`)
+    }
+}   
+const getJobOfferById = (req, res) => {
+    const { id } = req.params
+    const findJobOffer = JobOffer.findIndex(JobOffer => JobOffer.id == id);
+    //console.log(findJobOffer)
+    if (findJobOffer >= 0) {
+        return res.status(200).json(JobOffer[findJobOffer])
+    } 
+        return res.status(404).json('No existe videojuego con esa id')
+    
+}
+
+module.exports = {
+    getJobOffer, 
+    postNewJobOffer,
+    putJobOffer,
+    pachJobOffer,
+    deleteJobOffer,
+    getJobOffer
+};

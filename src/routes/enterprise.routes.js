a55b5a820f1648dff8268515be3552a9335a8147
“isaMoratogit config --global user.email “morato.delacalle@bootcamp-upgrade.com <morato.delacalle@bootcamp-upgrade.com>
2021-11-13 16:29:21 +0100

    FEAT: entrerprise routes

--- src/routes/enterprise.routes.js
+++ src/routes/enterprise.routes.js
@@ -1,8 +1,16 @@
 const EnterpriseRoutes = require("express").Router();
 
-const {getEnterprises} = require('../controllers/enterprise.controller');
+const {getEnterprises,postNewEnterprises, putEnterprise, pachEnterprise, deleteEnterprise, getEnterpriseById} = require('../controllers/enterprise.controller');
 
 EnterpriseRoutes.get('/',getEnterprises)
+EnterpriseRoutes.post('/',postNewEnterprises)
+EnterpriseRoutes.put('/:id',putEnterprise)
+EnterpriseRoutes.patch('/:id',pachEnterprise)
+EnterpriseRoutes.delete('/:id',deleteEnterprise)
+EnterpriseRoutes.get('/:id',getEnterpriseById)
+
+
+
 
 
 module.exports = EnterpriseRoutes

94f84d94ed4be8aef799460522dba59b55d15fb2
“isaMoratogit config --global user.email “morato.delacalle@bootcamp-upgrade.com <morato.delacalle@bootcamp-upgrade.com>
2021-11-14 17:51:24 +0100

    FEAT: user auth

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,5 @@
 const User = require('../models/user.model')
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
+
 
 
 const getAllUsers = async (req, res, next) => {
@@ -24,48 +23,9 @@ const postNewUser = async (req, res, next) =>{
     }
 }
 
-const loginUser = async (req, res, next) =>{
-    try{
-        //console.log('alias-->',req.body.alias)
-        const userInBd = await User.findOne({alias:req.body.alias})
-       // console.log('usuario encontrado-->',userInBd)
-        
-       if(!userInBd){
-            const error = new Error
-            error.status = 404
-            error.message = 'No existe usuario con ese alias'
-            return next(error)
-        }
-        
-        if(bcrypt.compareSync(req.body.password, userInBd.password)){
-            userInBd.password = null
-           // console.log('constraseña correcta')
-
-            const token = jwt.sign({ id: userInBd._id, alias: userInBd.alias }, process.env.JWT_SECRET, { expiresIn: '1d' });
-
-            return res.status(200).json(token)
-        }
-
-    }catch(error){
-        error.message = 'error al loguear'
-        return next(error)
-    }
-
-}
-
-const logoutUser = (req, res, next) => {
-    try{
-        const token = null
-        return res.status(200).json(token)
-    }catch(error){
-        return next(error)
-    }
-}
 
 module.exports = {
     getAllUsers,
-    postNewUser,
-    loginUser,
-    logoutUser
-
+    postNewUser
+   
 }

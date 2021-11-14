22a2f14ee0f43a3d3d9383ad7d6bdb6cab73a9ce
“isaMoratogit config --global user.email “morato.delacalle@bootcamp-upgrade.com <morato.delacalle@bootcamp-upgrade.com>
2021-11-14 17:53:35 +0100

    FEAT routes auth

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,13 +1,10 @@
 const userRoutes = require('express').Router();
-const {isAuth} = require('../middleware/auth.middleware')
 
-const {getAllUsers, postNewUser, loginUser, logoutUser} = require('../controllers/user.controller')
+const {getAllUsers, postNewUser} = require('../controllers/user.controller')
 
 userRoutes.get('/', getAllUsers)
 userRoutes.post('/', postNewUser)
 
-userRoutes.post('/login', loginUser)
-userRoutes.post('/logout', [isAuth], logoutUser)
 
 
 module.exports = userRoutes

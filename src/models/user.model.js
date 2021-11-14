1daa675c135d616c9f20339ffd0f36de2e7c3b19
“isaMoratogit config --global user.email “morato.delacalle@bootcamp-upgrade.com <morato.delacalle@bootcamp-upgrade.com>
2021-11-14 18:02:26 +0100

    FEAT: user authe

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -3,8 +3,10 @@ const mongoose = require('mongoose')
 const userSchema = new mongoose.Schema(
     {
        name:{type:String, trim:true, required:true},
-       alias: {type:String, trim:true, unique:true, required:true},
-       password:{type:String, trim:true,required:true}
+       CIF: {type:String, trim:true, unique:true, required:true},
+       email:{type:String, trim:true,required:true},
+       password:{type:String, trim:true,required:true},
+
        
     },
     {

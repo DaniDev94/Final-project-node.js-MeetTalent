20d92a0ddfa2c3cdb87a710c3572693ac6269e03
“isaMoratogit config --global user.email “morato.delacalle@bootcamp-upgrade.com <morato.delacalle@bootcamp-upgrade.com>
2021-11-15 13:52:13 +0100

    FEAT changes CEO

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const userSchema = new mongoose.Schema(
     {
         name: { type: String, trim: true, required: true },
-        CO:  {type:String, required: true, unique:true, trim:true},
+        CEO:  {type:String, required: true, unique:true, trim:true},
         image: {type:String, trim:true},
         CIF: { type: String, trim: true, unique: true, required: true },
         email: { type: String, trim: true, required: true, unique: true },

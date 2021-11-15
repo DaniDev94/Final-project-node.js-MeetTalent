97938988988316d171fe9d231732031a8fa1043a
Daniel López <daniel.lp1094@gmail.com>
2021-11-15 13:44:32 +0100

    FEAT: user model finished

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -3,10 +3,12 @@ const mongoose = require('mongoose')
 const userSchema = new mongoose.Schema(
     {
         name: { type: String, trim: true, required: true },
+        CO:  {type:String, required: true, unique:true, trim:true},
+        image: {type:String, trim:true},
         CIF: { type: String, trim: true, unique: true, required: true },
-        email: { type: String, trim: true, required: true },
-        password: { type: String, trim: true, required: true, unique: true },
-        joboffer: { type: mongoose.Types.ObjectId, ref: 'joboffer'}
+        email: { type: String, trim: true, required: true, unique: true },
+        password: { type: String, trim: true, required: true },
+        
     },
     {
         timestamps: true

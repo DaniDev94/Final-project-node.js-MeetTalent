92c2088d32a2bf37f64251b2a7563b008c3f73de
Daniel López <daniel.lp1094@gmail.com>
2021-11-15 17:48:01 +0100

    FEAT: candidate routes finished

--- src/routes/candidate.routes.js
+++ src/routes/candidate.routes.js
@@ -1,13 +1,9 @@
 const CandidateRoutes = require('express').Router();
-const upload = require('../middlewares/file.middleware');
-
-const { getAllCandidates, getCandidateById, postNewCandidate, putCandidateUpdate, deleteCandidate } = require('../controllers/candidate.controller');
+const { getAllCandidates, getCandidateById, deleteCandidate } = require('../controllers/candidate.controller');
 
 
 CandidateRoutes.get('/', getAllCandidates);
 CandidateRoutes.get('/:id', getCandidateById);
-CandidateRoutes.post('/', upload.single('image'), postNewCandidate);
-CandidateRoutes.put('/:id', putCandidateUpdate);
 CandidateRoutes.delete('/:id', deleteCandidate);
 
 

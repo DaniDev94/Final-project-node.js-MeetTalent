12c9478342b251c8097a502397250e834952590d
Daniel López <daniel.lp1094@gmail.com>
2021-11-15 13:30:14 +0100

    FEAT: user controller finished

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,8 +3,8 @@ const User = require('../models/user.model')
 
 const getAllUsers = async (req, res, next) => {
     try {
-        const allUsers = await User.find().populate('joboffer');
-        return res.status(200).json(allUsers)
+        const allUsers = await User.find();
+        return res.status(200).json(allUsers);
     } catch (err) {
         err.message = 'Users not found';
         return next(`Error: ${err}.`);
@@ -12,11 +12,23 @@ const getAllUsers = async (req, res, next) => {
 }
 
 
+const getUserById = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const userById = await User.findById(id);
+        return res.status(200).json(userById);
+    } catch (err) {
+        err.message = 'No user found with this id';
+        return next(`Error: ${err}.`);
+    }
+}
+
+
 const postNewUser = async (req, res, next) => {
     try {
-        const newUser = new User(req.body)
-        const userSave = await newUser.save()
-        return res.status(201).json(userSave)
+        const newUser = new User(req.body);
+        const saveUser = await newUser.save();
+        return res.status(200).json(saveUser);
     } catch (err) {
         err.message = 'The new user cannot be created, it may already exist';
         return next(`Error: ${err}.`);
@@ -37,18 +49,6 @@ const putUserUpdate = async (req, res, next) => {
     }
 }
 
-const patchAddOffer = async (req, res, next) => {
-    try {
-        const { id } = req.params;
-        const { idOffer } = req.body
-        const updateUserAddingOffer = await User.findByIdAndUpdate(id,{$push:{joboffer: idOffer}});
-        res.status(200).json(updateUserAddingOffer);
-    } catch (err) {
-        err.message = 'Job Offer not found, cannot be added to the User';
-        return next(`Error: ${err}.`);
-    }
-}
-
 
 const deleteUser = async (req, res, next) => {
     try {
@@ -56,6 +56,7 @@ const deleteUser = async (req, res, next) => {
         const deleteUser = await User.findByIdAndDelete(id);
         return res.status(200).json(deleteUser);
     } catch (err) {
+        err.status = 404;
         err.message = 'User not found, could not be removed';
         return next(`Error: ${err}.`);
     }
@@ -63,9 +64,9 @@ const deleteUser = async (req, res, next) => {
 
 
 module.exports = {
+    getUserById,
     getAllUsers,
     postNewUser,
     putUserUpdate,
-    patchAddOffer,
     deleteUser
 }

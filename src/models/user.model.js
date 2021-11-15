a66c88b3f2d0f3f6e27d055bb940c24d6748c410
Daniel López <daniel.lp1094@gmail.com>
2021-11-15 09:23:29 +0100

    FEAT: user routes finished

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -2,17 +2,16 @@ const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema(
     {
-       name:{type:String, trim:true, required:true},
-       CIF: {type:String, trim:true, unique:true, required:true},
-       email:{type:String, trim:true,required:true},
-       password:{type:String, trim:true,required:true},
-
-       
+        name: { type: String, trim: true, required: true },
+        CIF: { type: String, trim: true, unique: true, required: true },
+        email: { type: String, trim: true, required: true },
+        password: { type: String, trim: true, required: true, unique: true },
+        joboffer: { type: mongoose.Types.ObjectId, ref: 'joboffer'}
     },
     {
         timestamps: true
     }
 );
 
-const User = mongoose.model('users', userSchema)
-module.exports = User
+const User = mongoose.model('users', userSchema);
+module.exports = User;

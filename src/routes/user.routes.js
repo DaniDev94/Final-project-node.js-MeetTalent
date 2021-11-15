9434429e6f9467a438d6f6d4f6c5478f64efabc7
Daniel López <daniel.lp1094@gmail.com>
2021-11-15 09:19:36 +0100

    FEAT: user routes finished

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,12 @@
 const userRoutes = require('express').Router();
-
-const {getAllUsers, postNewUser} = require('../controllers/user.controller')
-
-userRoutes.get('/', getAllUsers)
-userRoutes.post('/', postNewUser)
+const {getAllUsers, postNewUser, putUserUpdate, patchAddOffer, deleteUser} = require('../controllers/user.controller');
 
 
+userRoutes.get('/', getAllUsers);
+userRoutes.post('/', postNewUser);
+userRoutes.put('/:id', putUserUpdate);
+userRoutes.patch('/addoffer/:id', patchAddOffer);
+userRoutes.delete('/:id', deleteUser);
 
-module.exports = userRoutes
+
+module.exports = userRoutes;

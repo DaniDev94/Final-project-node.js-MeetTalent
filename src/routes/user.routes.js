0b989fc0d671ea18d2ddacd908ef012736f4f265
Daniel López <daniel.lp1094@gmail.com>
2021-11-15 17:48:35 +0100

    FEAT: user routes finished

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,11 @@
 const userRoutes = require('express').Router();
+const upload = require('../middlewares/file.middleware');
 const {getAllUsers, getUserById, postNewUser, putUserUpdate, deleteUser} = require('../controllers/user.controller');
 
 
-userRoutes.get('/', getAllUsers);
+userRoutes.get('/', getAllUsers);//Eliminar al sacar la BBDD a producción
 userRoutes.get('/:id', getUserById);
-userRoutes.post('/',  postNewUser);
+userRoutes.post('/', upload.single('image'), postNewUser);
 userRoutes.put('/:id', putUserUpdate);
 userRoutes.delete('/:id', deleteUser);
 

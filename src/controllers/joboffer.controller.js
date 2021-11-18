ff7ab6a1e65cbec0c4e81da964548735bfd3aeac
Daniel López <daniel.lp1094@gmail.com>
2021-11-18 14:49:11 +0100

    FEAT: add new method get and clean clode

--- src/controllers/joboffer.controller.js
+++ src/controllers/joboffer.controller.js
@@ -4,62 +4,63 @@ const JobOffer = require('../models/joboffer.models');
 const getJobOfferByUser = async (req, res, next) => {
     try {
         const idUser = req.body.user;
-        const findOfferts = await JobOffer.find({user: idUser});
+        const findOfferts = await JobOffer.find({ user: idUser });
         return res.status(200).json(findOfferts);
     } catch (err) {
-        err.message = 'Not found';
-        return next (`Error:${err}`)
+        err.message = 'There are no offers in this user';
+        return next(`Error:${err}`)
     }
 }
 
 
-const getJobOfferById = async (req, res) => {
-    const { id } = req.params
-    const findJobOffer = JobOffer.findIndex(JobOffer => JobOffer.id == id);
-    if (findJobOffer >= 0) {
-        return res.status(200).json(JobOffer[findJobOffer])
-    } 
-        return res.status(404).json('No existe oferta con esa id')
+const getJobOfferById = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const offerById = await JobOffer.findById(id);
+        return res.status(200).json(offerById);
+    } catch (err) {
+        err.message = 'No job offer found with this id';
+        return next(`Error: ${err}.`);
+    }
 }
 
 
-const postNewJobOffer = async(req, res, next) => {
-    try{
+const postNewJobOffer = async (req, res, next) => {
+    try {
         const newJoboffer = new JobOffer(req.body);
         const JobOffersave = await newJoboffer.save()
         return res.status(200).json(JobOffersave)
-    }catch(err){
+    } catch (err) {
         err.message = 'The new hob offer cannot created';
-        return next (`Error:${err}`)
+        return next(`Error:${err}`)
     }
 }
 
 
 const putJobOffer = async (req, res, next) => {
-    try{
-        const {id} = req.params;
-
-        const otherJobOffer=new JobOffer(req.body);
+    try {
+        const { id } = req.params;
+        const otherJobOffer = new JobOffer(req.body);
         otherJobOffer._id = id;
-        const updateJobOffer= await JobOffer.findByIdAndUpdate(id, otherJobOffer);
+        const updateJobOffer = await JobOffer.findByIdAndUpdate(id, otherJobOffer);
         return res.status(200).json(updateJobOffer);
-    }catch(err){
+    } catch (err) {
         err.message = 'Job Offer cannot be update';
-        return next (`Error:${err}`)
+        return next(`Error:${err}`)
     }
-}  
+}
 
 
-const deleteJobOffer =  async (req, res, next) => {
-    try{
-        const {id} = req.params;
-        const deleteJobOffer= await JobOffer.findByIdAndDelete(id)
+const deleteJobOffer = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const deleteJobOffer = await JobOffer.findByIdAndDelete(id)
         return res.status(200).json(deleteJobOffer);
-    }catch(err){
-        err.message = 'JobOffer cannot be update';
-        return next (`Error:${err}`)
+    } catch (err) {
+        err.message = 'Job Offer cannot be deleted';
+        return next(`Error:${err}`)
     }
-}   
+}
 
 
 module.exports = {

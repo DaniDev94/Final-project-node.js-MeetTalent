28af62cd4fe84ba13cb474711051c85ae025a86b
“isaMoratogit config --global user.email “morato.delacalle@bootcamp-upgrade.com <morato.delacalle@bootcamp-upgrade.com>
2021-11-20 23:22:57 +0100

    quitar y

--- src/main.js
+++ src/main.js
@@ -21,7 +21,7 @@ const PORT = 4000;
 const app = express();
 connectWithDb();
 
-app.use((req, res, next) => {y
+app.use((req, res, next) => {
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH');
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Headers', 'Content-Type');
